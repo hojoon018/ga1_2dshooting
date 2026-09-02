a5b8ee7bbc11d29beea1a350fe2edacf90fe0b3d
hojoon018 <skewed018@gmail.com>
2026-09-02 15:44:32 +0900

    feat: 총알 자동/수동 모드 토글 기능

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -13,23 +13,41 @@ public class PlayerFire : MonoBehaviour
     public bool canFire = true;
     public float coolTime = 1f;
 
+    public bool isAutoFire = false;
+
     private float currentCoolTime = 1f;
     
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            isAutoFire = !isAutoFire;
+        }
         CheckCoolTime(Time.deltaTime);
         if (canFire)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (isAutoFire)
             {
-                // 2. 총알 프리팹을 생성한다.
-                // Instantiate는 프리팹으로부터 복사해서 (Monobehaviour를 상속받는) 게임 오브젝트를 생성하고 씬에 넣어주는 기능
-                GameObject rightBullet = Instantiate(BulletPrefab);
-                rightBullet.transform.position = RightFirePoint.position; // 생성한 총알의 위치를 나(플레이어)의 위치로
+                GameObject autoRightBullet = Instantiate(BulletPrefab);
+                autoRightBullet.transform.position = RightFirePoint.position; // 생성한 총알의 위치를 나(플레이어)의 위치로
             
-                GameObject leftBullet = Instantiate(BulletPrefab);
-                leftBullet.transform.position = LeftFirePoint.position;
+                GameObject autoLeftBullet = Instantiate(BulletPrefab);
+                autoLeftBullet.transform.position = LeftFirePoint.position;
                 canFire = false;
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    // 2. 총알 프리팹을 생성한다.
+                    // Instantiate는 프리팹으로부터 복사해서 (Monobehaviour를 상속받는) 게임 오브젝트를 생성하고 씬에 넣어주는 기능
+                    GameObject rightBullet = Instantiate(BulletPrefab);
+                    rightBullet.transform.position = RightFirePoint.position; // 생성한 총알의 위치를 나(플레이어)의 위치로
+            
+                    GameObject leftBullet = Instantiate(BulletPrefab);
+                    leftBullet.transform.position = LeftFirePoint.position;
+                    canFire = false;
+                }
             }
         }
         
