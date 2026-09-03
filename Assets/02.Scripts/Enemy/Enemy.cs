3431648c86bb48d443e38d421d3455651b8431a6
hojoon018 <skewed018@gmail.com>
2026-09-03 13:16:44 +0900

    feat: Enemy 태그 추가 및 총알 충돌시 Enemy일 경우에만 파괴

--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -6,7 +6,7 @@ public class Enemy : MonoBehaviour
 
     private void Update()
     {
-        Vector2 direction = Vector2.up; // new Vector2(1, 0);
+        Vector2 direction = Vector2.down; // new Vector2(1, 0);
         transform.Translate(direction * speed * Time.deltaTime);
     }
 }
