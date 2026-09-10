5a934ecec66b65581028469f36e06eac2269e022
hojoon018 <skewed018@gmail.com>
2026-09-10 14:17:21 +0900

    perf: 더티 플래그를 이용한 UI 갱신 최적화
    - 마지막 갱신 점수와 차이가 없다면 text 안 바꿈

--- Assets/02.Scripts/ScoreManager.cs
+++ Assets/02.Scripts/ScoreManager.cs
@@ -12,7 +12,8 @@ public class ScoreManager : MonoBehaviour
 
     // 관리 : 특정 데이터에 대한 무결성과 생성, 읽기, 수정, 삭제 등과 관련된 게임 로직
     private int _bestScore;
-    private int _currentScore;
+    private int _currentScore = 0;
+    private int _lastRefreshScore = -1;
 
     // UI 책임 추가 (텍스트메시 프로 참조)
     [SerializeField] private TextMeshProUGUI _bestScoreTextUI;
@@ -21,11 +22,12 @@ public class ScoreManager : MonoBehaviour
     private void Awake()
     {
         // 늦게 태어난 매니저는 나는 늦었네~ 하면서 삭제
-        if (Instance != null)
+        if (_instance != null)
         {
             Destroy(gameObject);
             return;
         }
+
         _instance = this;
     }
 
@@ -47,7 +49,11 @@ public class ScoreManager : MonoBehaviour
 
     private void Refresh()
     {
+        if (_lastRefreshScore == _currentScore) return;
+
         _bestScoreTextUI.text = $"BestScore: {_bestScore}";
         _currentScoreTextUI.text = $"Score: {_currentScore}";
+
+        _lastRefreshScore = _currentScore;
     }
 }
