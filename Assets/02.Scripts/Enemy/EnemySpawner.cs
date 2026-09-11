9ae41ef39bf4aa4b2d9831df76f77ba919730cf1
hojoon018 <skewed018@gmail.com>
2026-09-11 10:32:47 +0900

    feat: 에너미 스폰: 가중치 기반 랜덤 선택
    - EnemySpawnData

--- Assets/02.Scripts/Enemy/EnemySpawner.cs
+++ Assets/02.Scripts/Enemy/EnemySpawner.cs
@@ -8,8 +8,7 @@ public class EnemySpawner : MonoBehaviour
     [Header("스폰 간격")][SerializeField] private float _spawnInterval = 3f;
     private float _timer;
 
-    // - 생성할 프리팹
-    [Header("스폰할 적 프리팹")][SerializeField] private Enemy[] _enemyPrefabs;
+    [SerializeField] private EnemySpawnData[] _spawnDatas;
 
 
     private void Update()
@@ -30,26 +29,37 @@ public class EnemySpawner : MonoBehaviour
     {
         // 각 스포너가 적을 스폰할 때 확률에 따라 다른 타입의 적을 스폰해주세요.
 
-        int enemyPrefabIndex = 0;
-        int randomPercent = Random.Range(0, 100);
-
         // Todo: Sciptable Object를 사용해서 리팩토링
         // 이유 1 : 배열을 사용했지만 각 아이템이 어떤 프리팹인지 알 수가 없음
         // 이유 2 : 각 에너미 스폰 확률을 매직 넘버로 하드코딩해서 유지보수가 어렵다.
-        if (randomPercent < 50)
+
+
+        // 가중치 랜덤 선택
+        // 각 아이템에 가중치를 부여하고, 가중치가 클수록 높은 확률로 선택되도록 하는 방식
+
+        // 1. 추첨할 수 있는 모든 가중치를 더한다.
+
+        int totalWeight = 0;
+        foreach (EnemySpawnData data in _spawnDatas)
         {
-            enemyPrefabIndex = 0;
-        }
-        else if (randomPercent < 80)
-        {
-            enemyPrefabIndex = 1;
-        }
-        else
-        {
-            enemyPrefabIndex = 2;
+            totalWeight += data.Weight;
         }
 
-        Enemy enemy = Instantiate(_enemyPrefabs[enemyPrefabIndex]);
-        enemy.transform.position = transform.position;
+        // 2. 전체 가중치 범위에서 랜덤한 정수를 뽑는다.
+        int randomWeight = Random.Range(0, totalWeight);
+
+        // 3. 가중치를 누적하면서 선택된 구간을 찾는다.
+        int cumulativeWeight = 0;
+
+        foreach (EnemySpawnData data in _spawnDatas)
+        {
+            cumulativeWeight += data.Weight;
+            if (randomWeight < cumulativeWeight)
+            {
+                GameObject enemy = Instantiate(data.EnemyPrefab);
+                enemy.transform.position = transform.position;
+                break;
+            }
+        }
     }
 }
