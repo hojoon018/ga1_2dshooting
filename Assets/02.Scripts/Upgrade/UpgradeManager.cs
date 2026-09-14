14715f87b3c272d9f0e035e4e48e9384ef26de2e
hojoon018 <skewed018@gmail.com>
2026-09-14 14:52:33 +0900

    feat: 업그레이드 시스템
    - Upgrade, UpgradeManager, UI_Upgrade

---
+++ Assets/02.Scripts/Upgrade/UpgradeManager.cs
@@ -0,0 +1,48 @@
+using TMPro;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour
+{
+    // 업그레이드 관리자 : 업그레이드들에 대한 무결성과 생성, 조회, 수정, 삭제 등과 관련된 게임 로직
+    private static UpgradeManager _instance;
+    public static UpgradeManager Instance => _instance;
+
+
+    // 업그레이드 도메인 클래스
+    [SerializeField] private Upgrade[] _upgrades;
+    public Upgrade[] Upgrades => _upgrades;
+
+    // 업그레이드 UI들
+    [SerializeField] private UI_Upgrade[] _uiUpgrades;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
+    }
+
+    private void Start()
+    {
+        RefreshUI();
+    }
+
+    public void LevelUp(int index)
+    {
+        _upgrades[index].LevelUp();
+
+        RefreshUI();
+    }
+
+    public void RefreshUI()
+    {
+        foreach (UI_Upgrade uiUpgrade in _uiUpgrades)
+        {
+            uiUpgrade.Refresh();
+        }
+    }
+}
