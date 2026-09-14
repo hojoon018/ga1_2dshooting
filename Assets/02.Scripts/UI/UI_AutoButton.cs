6d03f2ad08dac510845c025ab25d880e026de7d1
hojoon018 <skewed018@gmail.com>
2026-09-14 11:09:49 +0900

    fix: 버튼 범프 스케일
    - Lerp에서 시작값 잘못

--- Assets/02.Scripts/UI/UI_AutoButton.cs
+++ Assets/02.Scripts/UI/UI_AutoButton.cs
@@ -1,5 +1,7 @@
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.UI;
+using Vector3 = UnityEngine.Vector3;
 
 public class UI_AutoButton : MonoBehaviour
 {
@@ -72,7 +74,7 @@ public class UI_AutoButton : MonoBehaviour
         // 2. 누적 시간과 애니메이션 커브에 따른 스케일 변경
         float time = _elapsedTime / BumpDuration; // 얼마나 지났는지 퍼센트 (0 ~ 1)
         float curveValue = _bumpCurve.Evaluate(time); // 퍼센트에 따라 커브 애니메이션 값 추출
-        transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one * BumpScale, curveValue);
+        transform.localScale = Vector3.Lerp(Vector3.one, Vector3.one * BumpScale, curveValue);
     }
     // 사운드: 일레븐랩스에서 버튼 클릭 공용 사운드 만들어서 적용
 
